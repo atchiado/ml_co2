41e3a4e34758544d069fb81fae84ea0da787fecb
Anthony Chiado <anthonychiado97@gmail.com>
2022-10-05 17:00:33 -0400

    begin modelling

--- co2_modeling_script.R
+++ co2_modeling_script.R
@@ -11,7 +11,8 @@ ml_co2_data <- read.csv(file = "~/Desktop/work/data/r/ml_co2/CO2_emission.csv")
 ## DATA PREP ----------
 co2_data <- ml_co2_data %>%
   select(-c(Model_Year, Make, Model)) %>%
-  mutate_if(is.ordered, factor, ordered = FALSE)
+  mutate_if(is.ordered, factor, ordered = FALSE) %>%
+  subset()
 
 set.seed(123)
 
@@ -19,5 +20,17 @@ set.seed(123)
 co2_split <- createDataPartition(y = co2_data$Vehicle_Class, p = 0.7, list = FALSE)
 co2_train <- co2_data[co2_split, ]
 co2_test <- co2_data[-co2_split, ]
+
+# check distribution in training and test sets
+prop.table(table(co2_train$Vehicle_Class)) * 100
+prop.table(table(co2_test$Vehicle_Class)) * 100
+prop.table(table(co2_data$Vehicle_Class)) * 100
+
+# normalizing and scaling variables
+control <- trainControl(method = "repeatedcv", repeats = 3)
+knn_fit <- train(Vehicle_Class ~ ., data = co2_train, method = "knn", trControl = control,
+                preProcess = c("center","scale"), tuneLength = 20)
+
+
 
 
